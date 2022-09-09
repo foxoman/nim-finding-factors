de767839479d4a329ad38b73ebd2314295c70766
Sultan Al Isaiee <sultan@foxoman.net>
2022-09-09 23:07:03 +0400

    Update main.nim

--- main.nim
+++ main.nim
@@ -1,24 +1,18 @@
-#? replace(sub = "\t", by = " ")
 from strutils import parseInt
+import terminal, std/rdstdin
 
-echo "\n" & "\e[1m" & "Find factors for any given number with bruteforce!" & "\e[0m"
-echo "\e[2m" & "I experienced difficulty in setting the text output colour, unlike in rust" & "\e[0m"
+styledEcho fgMagenta, "[**] Find factors for any given number with bruteforce!", resetStyle
 
 var num: int
 while true:
-	echo "Please input your number:"
-	var input = readLine(stdin)
-	try: 
-		num = parseInt(input)
+	try:
+		num = parseInt readLineFromStdin("[*] Please input your number:")
 		break
 	except:
-		echo "That is not a valid number!"
+		styledEcho fgRed, "[x] That is not a valid number! Try again.", resetStyle
 
-echo ""
 for i in 1..num+1:
 	if num mod i == 0:
 		echo $(num div i) & " x " & $i
 
-echo "\e[2m" & "All factors have been found" & "\e[0m"
-
-# nim c -r main.nim
+styledEcho fgGreen, "[*] All factors have been found.", resetStyle
